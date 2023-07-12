a9edc2d2de4396e4d97764902f09c18a1b387d83
Dasun Tharaka <wkdasunt0001@gmail.com>
2023-07-12 23:34:11 +0530

    validation done

--- back-end/src/main/java/lk/ijse/dep10/backend/api/CustomerController.java
+++ back-end/src/main/java/lk/ijse/dep10/backend/api/CustomerController.java
@@ -4,6 +4,7 @@ import lk.ijse.dep10.backend.dto.CustomerDTO;
 import lk.ijse.dep10.backend.dto.CustomerLoggingDTO;
 import lk.ijse.dep10.backend.service.CustomerService;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -18,12 +19,12 @@ public class CustomerController {
     // Endpoint to save a customer
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/save")
-    public void saveCustomer(@RequestBody CustomerDTO customerDTO){
+    public void saveCustomer(@RequestBody @Validated CustomerDTO customerDTO){
         customerService.saveCustomer(customerDTO);
     }
     // Endpoint for customer logging
     @PostMapping
-    public CustomerDTO customerLogging(@RequestBody CustomerLoggingDTO customerLoggingDTO){
+    public CustomerDTO customerLogging(@RequestBody @Validated CustomerLoggingDTO customerLoggingDTO){
         return customerService.customerLogging(customerLoggingDTO);
 
     }
--- back-end/src/main/java/lk/ijse/dep10/backend/dto/CustomerDTO.java
+++ back-end/src/main/java/lk/ijse/dep10/backend/dto/CustomerDTO.java
@@ -4,15 +4,21 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class CustomerDTO {
     private int id;
+    @NotBlank(message = "Name can't be empty")
     private String name;
+    @NotBlank(message = "Email can't be empty")
     private String email;
+    @NotBlank(message = "Password can't be empty")
     private String password;
+    @NotBlank(message = "GroupId can't be empty")
     private int groupId;
 
     public CustomerDTO(String name, String email, String password, int groupId) {
